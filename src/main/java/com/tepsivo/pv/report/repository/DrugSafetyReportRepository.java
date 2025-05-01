4397780444f1c4aac755cf8be7d021109ad5cee5
Asraful Forhad Chowdhury <asraful.cse@gmail.com>
2025-05-01 23:02:26 +0300

    infra directory introduced

--- src/main/java/com/tepsivo/pv/report/repository/DrugSafetyReportRepository.java
+++ src/main/java/com/tepsivo/pv/report/repository/DrugSafetyReportRepository.java
@@ -2,7 +2,6 @@ package com.tepsivo.pv.report.repository;
 
 import com.tepsivo.pv.report.web.api.dto.ReportStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
