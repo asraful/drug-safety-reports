055b86da7c13c626514f5aba3a01fe01a74cae86
Asraful Forhad Chowdhury <asraful.cse@gmail.com>
2025-05-01 19:10:01 +0300

    update ReportApplicationTests

--- src/test/java/com/tepsivo/pv/report/ReportApplicationTests.java
+++ src/test/java/com/tepsivo/pv/report/ReportApplicationTests.java
@@ -1,13 +1,22 @@
 package com.tepsivo.pv.report;
 
+import com.tepsivo.pv.report.web.api.endpoint.ReportController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class ReportApplicationTests {
 
+
+	@Autowired
+	private ReportController controller;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
